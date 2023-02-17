89a6001ac6828742cf497e1c7de5f9906db5d852
vyabc <125678532+vyabc@users.noreply.github.com>
2023-02-17 21:17:41 +0530

    Add files via upload

---
+++ templates/build/js/tempCodeRunnerFile.js
@@ -0,0 +1,52 @@
+
+// const open_btn = document.querySelector('.open-btn');
+// const close_btn = document.querySelector('.close-btn');
+// const popup = document.querySelector('.popup');
+// const main_popup = document.querySelector('.main-popup');
+
+// open_btn.addEventListener('click', () => {
+// 	popup.style.display = 'flex';
+// 	main_popup.style.cssText = 'animation:slide-in .5s ease; animation-fill-mode: forwards;';
+// });
+
+// close_btn.addEventListener('click', () => {
+// 	main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+// 	setTimeout(() => {
+// 		popup.style.display = 'none';
+// 	}, 500);
+// });
+
+// window.addEventListener('click', (e) => {
+// 	if (e.target == document.querySelector('.popup-overlay')) {
+// 		main_popup.style.cssText = 'animation:slide-out .5s ease; animation-fill-mode: forwards;';
+// 		setTimeout(() => {
+// 			popup.style.display = 'none';
+// 		}, 500);
+// 	}
+// });
+
+// const open_btn1 = document.querySelector('.open-btn1');
+// const close_btn1 = document.querySelector('.close-btn1');
+// const popup1 = document.querySelector('.popup1');
+// const main_popup1 = document.querySelector('.main-popup1');
+
+// open_btn1.addEventListener('click', () => {
+// 	popup.style.display = 'flex';
+// 	main_popup.style.cssText = 'animation:slide-in1 .5s ease; animation-fill-mode: forwards;';
+// });
+
+// close_btn1.addEventListener('click', () => {
+// 	main_popup1.style.cssText = 'animation:slide-out1 .5s ease; animation-fill-mode: forwards;';
+// 	setTimeout(() => {
+// 		popup1.style.display = 'none';
+// 	}, 500);
+// });
+
+// window.addEventListener('click', (e) => {
+// 	if (e.target == document.querySelector('.popup-overlay1')) {
+// 		main_popup1.style.cssText = 'animation:slide-out1 .5s ease; animation-fill-mode: forwards;';
+// 		setTimeout(() => {
+// 			popup1.style.display = 'none';
+// 		}, 500);
+// 	}
+// });
